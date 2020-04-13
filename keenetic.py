1c74e01e8e8415881d142e38fefc0e628405249b
olvint <59015286+olvint@users.noreply.github.com>
2020-04-13 19:01:51 +0300

    Create keenetic.py

---
+++ keenetic.py
@@ -0,0 +1,25 @@
+# Code for getting list of wifi clients (mac addresses) from your router
+# This code is made and tested for Keenetic Giga 2 (Keenetic Giga II)
+# Router firmware v2.06(AAFS.0)C3
+# I could only find a way to get arp cache which contains all clients connected via wifi and lan
+
+
+import telnetlib
+
+# Input data for your router
+HOST = "192.168.0.1"
+user = "#USER#"                                       # Please note that telnet is turned off by default 
+password = "#your password#"                          # you need to make a new account to access telnet (readonly is acceptable)
+
+tn = telnetlib.Telnet(HOST)                           # Connecting to host using standard Telnet port 23
+tn.read_until(b"Login: ")                             # Awaiting for router to ask for login (Prints "login: ")
+tn.write(user.encode('ascii') + b"\n")                # Writing your login
+tn.read_until(b"Password: ")                          # Awaiting for router to ask for Password
+tn.write(password.encode('ascii') + b"\n")            # Writing your password
+tn.read_until(b"(config)> ")                          # Awaiting for router to write greetings and be ready
+tn.write("show".encode('ascii') + b"\n")              # Sending command "show" to be able to input command
+tn.read_until(b"(show)> ")                            # Awaiting for router to ask for login (Note capital "L")
+tn.write("ip arp".encode('ascii') + b"\n")            # Sending command "ip arp" to be able to input command
+output= tn.read_until(b"(show)> ").decode('ascii')    # Awaiting for router to write wifi clients
+print (output)                                        # Print wifi clients
+tn.close()                                            # Close telnet connection
