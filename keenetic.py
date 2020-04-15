0526b3c3c9de48b1509ee034d62408c97f9e9abb
olvint <59015286+olvint@users.noreply.github.com>
2020-04-15 16:05:29 +0300

    Update keenetic.py

--- keenetic.py
+++ keenetic.py
@@ -5,6 +5,7 @@
 
 
 import telnetlib
+import re
 
 # Input data for your router
 HOST = "192.168.0.1"
@@ -18,8 +19,12 @@ tn.read_until(b"Password: ")                          # Awaiting for router to a
 tn.write(password.encode('ascii') + b"\n")            # Writing your password
 tn.read_until(b"(config)> ")                          # Awaiting for router to write greetings and be ready
 tn.write("show".encode('ascii') + b"\n")              # Sending command "show" to be able to input command
-tn.read_until(b"(show)> ")                            # Awaiting for router to ask for login (Note capital "L")
-tn.write("ip arp".encode('ascii') + b"\n")            # Sending command "ip arp" to be able to input command
-output= tn.read_until(b"(show)> ").decode('ascii')    # Awaiting for router to write wifi clients
-print (output)                                        # Print wifi clients
+tn.read_until(b"(config)> ")                          # Awaiting for router to write greetings and be ready
+tn.write("show associations".encode('ascii') + b"\n") # Sending command  show associations" to be able to input command
+output= tn.read_until(b"(config)> ").decode('ascii')  # Awaiting for router to write wifi clients
+# print (output)                                      # Print wifi clients
 tn.close()                                            # Close telnet connection
+# Getting list of Macs using regular expressions
+found = re.findall(re.compile(r'(?:[0-9a-fA-F]:?){12}'), output)
+for a in found:
+    print (a)
